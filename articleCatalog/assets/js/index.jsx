cc6b78e0da33cca101cb43aff3d1b0838f4813c9
Shanmuk <shanmuksastry@gmail.com>
2019-06-02 17:10:58 +0530

    change in project structure

--- articleCatalog/assets/js/index.jsx
+++
@@ -1,5 +0,0 @@
-var React = require('react');
-import App from './app.jsx';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<App/>, document.getElementById('react-app'));
---
+++ assets/js/index.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+import App from './app.jsx';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App/>, document.getElementById('react-app'));
